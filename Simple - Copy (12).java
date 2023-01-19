c367802c4661e2ab6d560295634f76341dcfb65d
narni54321 <122954876+narni54321@users.noreply.github.com>
2023-01-19 09:42:38 +0530

    Update Simple - Copy (12).java
    Added comments

--- Simple - Copy (12).java
+++ Simple - Copy (12).java
@@ -1,3 +1,7 @@
+//Author: Vinod
+//Date: 18-Jan-2023
+//Purpose of this Program: Just to test the build
+
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
